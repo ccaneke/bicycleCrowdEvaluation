17b54666852ee0dac465e2ce41050d0aa3b76b0c
Chukwuemezie Christopher Aneke <chukwuemezie.aneke.15@ucl.ac.uk>
2021-07-26 07:18:46 +0200

    improve design of code, and add functions to plot graphs for the balance of the reference dataset, good and bad annotators, and average annotation times of annotators

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 
 	"strconv"
 
+	"os"
+
 	"github.com/ccaneke/bicycleProjectCrowdEvaluation/bicycleCrowdEvaluator"
+	"github.com/wcharczuk/go-chart"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
@@ -13,29 +16,11 @@ import (
 )
 
 func PlotBarChartAnnotationTimes(v1 int, v2 int, v3 int) {
-	// values := plotter.Values{float64( /*v1*/ 100), float64( /*v2*/ 200), float64( /*v3*/ -300)}
-	// verticalLabels := []string{"Average, Min, Max"}
-	// //horizontalLabels := []string{"Average", "Min", "Max"}
-
-	// p1 := plot.New()
-	// verticalBarChart, err := plotter.NewBarChart(values, 0.5*vg.Centimeter)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-
-	// p1.Add(verticalBarChart)
-	// p1.NominalX(verticalLabels...)
-	// err = p1.Save(100, 100, "/tmp/verticalBarChart.png")
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	groupA := plotter.Values{float64(v1), float64(v2), float64(v3) /*-20, 35, 30, 35, 27*/}
-	//groupB := plotter.Values{25 /*, 32, 34, 20, 25*/}
-	//groupC := plotter.Values{12 /*, 28, 15, 21, 8*/}
+	groupA := plotter.Values{float64(v1), float64(v2), float64(v3)}
 
 	p := plot.New()
 
-	p.Title.Text = "Bar chart"
+	//p.Title.Text = "Bar chart"
 	p.Y.Label.Text = "Annotation times (milliseconds)"
 
 	w := vg.Points(20)
@@ -46,27 +31,9 @@ func PlotBarChartAnnotationTimes(v1 int, v2 int, v3 int) {
 	}
 	barsA.LineStyle.Width = vg.Length(0)
 	barsA.Color = plotutil.Color(0)
-	barsA.Offset = /*-w*/ 0
+	barsA.Offset = 0
 
-	// barsB, err := plotter.NewBarChart(groupB, w)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// barsB.LineStyle.Width = vg.Length(0)
-	// barsB.Color = plotutil.Color(1)
-
-	// barsC, err := plotter.NewBarChart(groupC, w)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// barsC.LineStyle.Width = vg.Length(0)
-	// barsC.Color = plotutil.Color(2)
-	// barsC.Offset = w
-
-	p.Add(barsA /*, barsB, barsC*/)
-	//p.Legend.Add("Group A", barsA)
-	// p.Legend.Add("Group B", barsB)
-	// p.Legend.Add("Group C", barsC)
+	p.Add(barsA)
 	p.Legend.Top = true
 	p.NominalX("Average", "Min", "Max")
 
@@ -76,84 +43,52 @@ func PlotBarChartAnnotationTimes(v1 int, v2 int, v3 int) {
 }
 
 func PlotBarChartAnnotatorResults(m map[string]int) {
-	var s []float64
-	for _, v := range m {
-		s = append(s, float64(v))
+	var m2 map[string]float64 = make(map[string]float64)
+
+	for k, v := range m {
+		m2[k] = float64(v)
 	}
 
-	groupA := plotter.Values{s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11],
-		s[12], s[13], s[14], s[15], s[16], s[17], s[18], s[19], s[20], s[21] /*float64(v1), float64(v2), float64(v3)*/ /*-20, 35, 30, 35, 27*/}
-	//groupB := plotter.Values{25 /*, 32, 34, 20, 25*/}
-	//groupC := plotter.Values{12 /*, 28, 15, 21, 8*/}
+	groupA := plotter.Values{m2["annotator_01"], m2["annotator_02"], m2["annotator_03"], m2["annotator_04"],
+		m2["annotator_05"], m2["annotator_06"], m2["annotator_07"], m2["annotator_08"], m2["annotator_09"],
+		m2["annotator_10"], m2["annotator_11"], m2["annotator_12"], m2["annotator_13"], m2["annotator_14"],
+		m2["annotator_15"], m2["annotator_16"], m2["annotator_17"], m2["annotator_18"], m2["annotator_19"],
+		m2["annotator_20"], m2["annotator_21"], m2["annotator_22"],
+	}
 
 	p := plot.New()
 
-	p.Title.Text = "Bar chart"
+	//p.Title.Text = "Bar chart"
 	p.Y.Label.Text = "Annotator results"
 	p.X.Label.Text = "Annotators"
 
 	w := vg.Points(20)
 
-	barsA, err := plotter.NewBarChart(groupA, w /**2*/)
+	barsA, err := plotter.NewBarChart(groupA, w)
 	if err != nil {
 		panic(err)
 	}
 	barsA.LineStyle.Width = vg.Length(0)
 	barsA.Color = plotutil.Color(0)
-	barsA.Offset = /*-w*/ 0
+	barsA.Offset = 0
 
-	// barsB, err := plotter.NewBarChart(groupB, w)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// barsB.LineStyle.Width = vg.Length(0)
-	// barsB.Color = plotutil.Color(1)
-
-	// barsC, err := plotter.NewBarChart(groupC, w)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// barsC.LineStyle.Width = vg.Length(0)
-	// barsC.Color = plotutil.Color(2)
-	// barsC.Offset = w
-
-	p.Add(barsA /*, barsB, barsC*/)
-	//p.Legend.Add("Group A", barsA)
-	// p.Legend.Add("Group B", barsB)
-	// p.Legend.Add("Group C", barsC)
+	p.Add(barsA)
 	p.Legend.Top = true
 	p.NominalX("1", "2", "3", "4", "5", "6",
 		"7", "8", "9", "10", "11", "12",
 		"13", "14", "15", "16", "17", "18",
 		"19", "20", "21", "22")
 
-	if err := p.Save( /*5*/ 10*vg.Inch /*3*/, 10*vg.Inch, "barchart2.png"); err != nil {
+	if err := p.Save(10*vg.Inch, 10*vg.Inch, "barchart2.png"); err != nil {
 		panic(err)
 	}
 }
 
 func PlotBarChartHighlyDisagreedQuestions(s []bicycleCrowdEvaluator.DisagreedQuestion) {
-	// groupA := plotter.Values{20, 35, 30, 35, 27}
-	// groupB := plotter.Values{25, 32, 34, 20, 25}
-	// groupC := plotter.Values{12, 28, 15, 21, 8}
-	// groupD := plotter.Values{20, 35, 30, 35, 27}
-	// groupE := plotter.Values{25, 32, 34, 20, 25}
-	// groupF := plotter.Values{12, 28, 15, 21, 8}
-
-	// var s2 []plotter.Values = make([]plotter.Values, len(s)* 2)
-
-	// var j int
-	// for i, v := range s {
-	// 	s2[]
-	// }
 	var first plotter.Values
 	var second plotter.Values
 	var xAxisNames []string
-	// for i := 0; i < len(s /*s2*/); i++ /*i += 2*/ {
-	// 	//s2[i] = plotter.Values{}
-	// 	first = append(first, float64(s[i].Votes.Yes))
-	// 	second = append(second, float64(s[i].Votes.No))
-	// }
+
 	for _, v := range s {
 		first = append(first, float64(v.Votes.Yes))
 		second = append(second, float64(v.Votes.No))
@@ -163,9 +98,9 @@ func PlotBarChartHighlyDisagreedQuestions(s []bicycleCrowdEvaluator.DisagreedQue
 	j := 16
 	k := 0
 	for i := 0; i < len(first); i += 16 {
-		firstTemp := first[i: /*16*/ j]
+		firstTemp := first[i:j]
 		secondTemp := second[i:j]
-		xAxisNamesTemp := xAxisNames[i: /*16*/ j]
+		xAxisNamesTemp := xAxisNames[i:j]
 
 		p := plot.New()
 
@@ -175,7 +110,7 @@ func PlotBarChartHighlyDisagreedQuestions(s []bicycleCrowdEvaluator.DisagreedQue
 
 		w := vg.Points(20)
 
-		barsA, err := plotter.NewBarChart(firstTemp /*groupA*/, w)
+		barsA, err := plotter.NewBarChart(firstTemp, w)
 		if err != nil {
 			panic(err)
 		}
@@ -183,31 +118,22 @@ func PlotBarChartHighlyDisagreedQuestions(s []bicycleCrowdEvaluator.DisagreedQue
 		barsA.Color = plotutil.Color(0)
 		barsA.Offset = -w
 
-		barsB, err := plotter.NewBarChart(secondTemp /*groupB*/, w)
+		barsB, err := plotter.NewBarChart(secondTemp, w)
 		if err != nil {
 			panic(err)
 		}
 		barsB.LineStyle.Width = vg.Length(0)
 		barsB.Color = plotutil.Color(1)
 
-		// barsC, err := plotter.NewBarChart(groupC, w)
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// barsC.LineStyle.Width = vg.Length(0)
-		// barsC.Color = plotutil.Color(2)
-		// barsC.Offset = w
-
-		p.Add(barsA, barsB /*, barsC*/)
-		p.Legend.Add( /*"Group A"*/ "Yes", barsA)
-		p.Legend.Add( /*"Group B"*/ "No", barsB)
-		//p.Legend.Add("Group C", barsC)
+		p.Add(barsA, barsB)
+		p.Legend.Add("Yes", barsA)
+		p.Legend.Add("No", barsB)
 		p.Legend.Top = true
 		p.Legend.XOffs = -10
-		p.NominalX( /*xAxisNames[0]*/ xAxisNamesTemp... /*"img"*/ /*"One"*/ /*, "Two", "Three", "Four", "Five"*/)
+		p.NominalX(xAxisNamesTemp...)
 
 		// Aspect ratio of 1.6667 is used for best results
-		if err := p.Save( /*5**/ 17* /*20**/ /*23**/ /*30**/ vg.Inch /*3*/ /*3**/, 10* /*12**/ /*14**/ /*10**/ vg.Inch, "/tmp/testdata/barchart"+strconv.Itoa(k)+".png"); err != nil {
+		if err := p.Save(17*vg.Inch, 10*vg.Inch, "/tmp/testdata/barchart"+strconv.Itoa(k)+".png"); err != nil {
 			panic(err)
 		}
 		k++
@@ -215,16 +141,97 @@ func PlotBarChartHighlyDisagreedQuestions(s []bicycleCrowdEvaluator.DisagreedQue
 	}
 }
 
+func PlotAverageAnnotationTimesForEachAnnotator(m map[string]float64) {
+	groupA := plotter.Values{m["annotator_01"], m["annotator_02"], m["annotator_03"], m["annotator_04"],
+		m["annotator_05"], m["annotator_06"], m["annotator_07"], m["annotator_08"], m["annotator_09"],
+		m["annotator_10"], m["annotator_11"], m["annotator_12"], m["annotator_13"], m["annotator_14"],
+		m["annotator_15"], m["annotator_16"], m["annotator_17"], m["annotator_18"], m["annotator_19"],
+		m["annotator_20"], m["annotator_21"], m["annotator_22"],
+	}
+
+	p := plot.New()
+
+	p.Title.Text = "Average annotation times for annotators"
+	p.Y.Label.Text = "Annotation times (milliseconds)"
+	p.X.Label.Text = "Annotators"
+
+	w := vg.Points(20)
+
+	barsA, err := plotter.NewBarChart(groupA, w)
+	if err != nil {
+		panic(err)
+	}
+	barsA.LineStyle.Width = vg.Length(0)
+	barsA.Color = plotutil.Color(0)
+	barsA.Offset = 0
+
+	p.Add(barsA)
+	p.Legend.Top = true
+	p.NominalX("1", "2", "3", "4", "5", "6",
+		"7", "8", "9", "10", "11", "12",
+		"13", "14", "15", "16", "17", "18",
+		"19", "20", "21", "22")
+
+	if err := p.Save(10*vg.Inch, 10*vg.Inch, "barchart5.png"); err != nil {
+		panic(err)
+	}
+}
+
+func PlotPieChart(d bicycleCrowdEvaluator.ReferenceDataSetResult) {
+	pie := chart.PieChart{
+		Width:  512,
+		Height: 512,
+		Values: []chart.Value{
+			chart.Value{Value: float64(d.IsBicycle), Label: "Bicycle Images: " + strconv.Itoa(d.IsBicycle)},
+			chart.Value{Value: float64(d.IsNotBicycle), Label: "Not bicycle images: " + strconv.Itoa(d.IsNotBicycle)},
+		},
+	}
+
+	f, _ := os.Create("output.png")
+	defer f.Close()
+	pie.Render(chart.PNG, f)
+}
+
+func PlotGoodOrBadAnnotators(annotators map[string]float32, title string) {
+	var group plotter.Values
+	var keys []string
+	for k, v := range annotators {
+		group = append(group, float64(v))
+		keys = append(keys, k)
+	}
+
+	p := plot.New()
+
+	p.Title.Text = title
+	p.Y.Label.Text = "Annotation scores (Percentage)"
+	p.X.Label.Text = "Annotators"
+
+	w := vg.Points(20)
+
+	barsA, err := plotter.NewBarChart(group, w*2)
+	if err != nil {
+		panic(err)
+	}
+	barsA.LineStyle.Width = vg.Length(0)
+	barsA.Color = plotutil.Color(2)
+	barsA.Offset = 0
+
+	p.Add(barsA)
+	p.Legend.Top = true
+	p.NominalX(keys...)
+
+	if err := p.Save(17*vg.Inch, 10*vg.Inch, "barchart"+title+".png"); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	var responses = bicycleCrowdEvaluator.Decode("/tmp/anonymized_project.json")
 
-	annotatorsInfo, response := bicycleCrowdEvaluator.Annotators(responses)
+	annotatorsInfo, response, cantSolveOrCorruptDataAnnotators, m2 := bicycleCrowdEvaluator.Annotators(responses)
 
 	fmt.Println("Task 1:")
-	//fmt.Println("AnnotatorInfo")
 	fmt.Printf("%+v", annotatorsInfo)
-
-	//fmt.Println(annotatorsInfo)
 
 	var yesNoAnswers map[string]*bicycleCrowdEvaluator.Votes = bicycleCrowdEvaluator.QuestionYesNoAnswers("/tmp/anonymized_project.json")
 
@@ -237,17 +244,24 @@ func main() {
 	fmt.Println()
 	fmt.Println("Task 2:")
 
+	var averageTimeOfAnnotators map[string]float64 = bicycleCrowdEvaluator.AverageTimeOfAnnotators(m2, annotatorsInfo)
 	fmt.Println("CantSolve:", response.CantSolve, "CorruptData:", response.CorruptData)
+	fmt.Println("Annotators who selected can't solve or corrupt data:", cantSolveOrCorruptDataAnnotators)
 	fmt.Println("Trend shows that annotators that selected true for cant_solve or corrupt_data had" +
-		"lower annotation times (duration), and of course didn't not answer yes or no")
+		" lower annotation times (duration) (when they chose either of these two options for a" +
+		"question), and of course didn't not answer yes or no")
+	fmt.Println("AverageTimeOfAllAnnotators:", averageTimeOfAnnotators)
+	PlotAverageAnnotationTimesForEachAnnotator(averageTimeOfAnnotators)
 
 	fmt.Println("Task 3:")
 	var referenceDataSetDistribution bicycleCrowdEvaluator.ReferenceDataSetResult = bicycleCrowdEvaluator.ReferenceDataSetDistribution("/tmp/references.json")
 
+	PlotPieChart(referenceDataSetDistribution)
 	fmt.Println("IsBicycle", referenceDataSetDistribution.IsBicycle, "IsNotBicycle",
 		referenceDataSetDistribution.IsNotBicycle, "\nReference Dataset is balanced because the number"+
 			" of images of bicycles and images that are not bicycles are approximately the same")
 
+	fmt.Println("Task 4:")
 	fmt.Println("Getting ReferenceSet...")
 	var referenceSet = bicycleCrowdEvaluator.GetReferenceSet("/tmp/references.json")
 
@@ -282,4 +296,6 @@ func main() {
 		annotatorsInfo.MaxAnnotationTimes)
 	PlotBarChartAnnotatorResults(annotatorsInfo.AnnotatorResults)
 	PlotBarChartHighlyDisagreedQuestions(disagreedQuestions)
+	PlotGoodOrBadAnnotators(good, "Good annotators")
+	PlotGoodOrBadAnnotators(bad, "Bad annotators")
 }
